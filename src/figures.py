64bb99f5779f023fb58a4c6c5e3fd8dd8af3b172
Fernando Siljestrom Berenguer <bochnan@hotmail.com>
2024-11-25 18:39:06 +0100

    App Final

--- src/figures.py
+++ src/figures.py
@@ -5,6 +5,7 @@ def create_keyword_bar_chart(keywords, song_title):
         x=[kw[0] for kw in keywords],
         y=[kw[1] for kw in keywords],
         labels={"x": "Palabra clave", "y": "Frecuencia"},
+        color_discrete_sequence=["#1DB954"],
         title=f"Palabras clave: {song_title}"
     )
     fig.update_layout(template="plotly_dark")
